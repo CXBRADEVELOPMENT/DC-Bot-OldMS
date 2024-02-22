71272a606996956410d8f19dba71a45472ea8d1d
C-DEVELOPMENT <nicokumar89@gmail.com>
2024-02-22 20:21:49 +0100

    Initial commit

---
+++ commands/utility_module/ping.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js")
+
+module.exports.run = async (bot, message, args) => {
+    message.delete();
+    let pingembed = new Discord.MessageEmbed()
+    .setDescription(`Ping: ${new Date().getTime() - message.createdTimestamp}ms`)
+    .setColor("#c88e38")
+    return message.channel.send(pingembed).then(msg => msg.delete({ timeout: 5000}));
+}
+
+module.exports.help = {
+    name: "ping"
+}
---
+++ start.sh
@@ -0,0 +1,1 @@
+screen -S DC-Bot node index.js
